dd00bcc9891374fec3862f4232a735b8cf25e352
vdcloud <vivekjdesai@gmail.com>
2016-06-22 14:21:15 +0530

    updated page title to reflect environment name

--- DeploymentDemo/Controllers/HomeController.cs
+++ DeploymentDemo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,7 @@ namespace DeploymentDemo.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = ConfigurationManager.AppSettings["InstanceName"].ToString();
             return View();
         }
 
